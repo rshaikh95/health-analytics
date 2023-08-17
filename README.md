56591cd952a97c65d39d48f644252d12a4e55e1d
rshaikh95 <itsrahil@live.com>
2023-08-17 15:21:15 +0000

    VariablesFunctions

---
+++ README.md
@@ -0,0 +1,2 @@
+# health-analytics
+This assignment is a primer for 504/507
---
+++ health_analysis.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+num = 1
+num2 = 5
+str = 'Rahil'
+
+my_list = [num, str, 2, "shaikh"]
+
+dict = {
+    "str2": "Rahil2",
+    "my_list2": [num, str, num2, "List"],
+    "NestD": {
+        'detail2':'something',
+        'detail3':'something2',
+        'detail4':'something3'
+    }
+}
+
+
+
+
+print(num2)
+print(str)
+print(my_list)
+print(dict)
+
