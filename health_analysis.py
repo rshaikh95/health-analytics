7d35c6a9b105f08f499ebf01eb098dce1f566769
rshaikh95 <itsrahil@live.com>
2023-08-18 00:28:08 +0000

    Cholesterol Analysis

--- health_analysis.py
+++ health_analysis.py
@@ -17,6 +17,20 @@ dict = {
     }
 }
 
+def Cholesterol(LDL, HDL):
+    # LDL
+    if LDL <= 100:
+        LDL_output = 'Heart-Healthy'
+    else:
+        LDL_output = 'Not Heart-Healthy'
+    # HDL 
+    if HDL >= 60:
+        HDL_output = 'Heart-Healthy'
+    else:
+        HDL_output = 'Not Heart-Healthy'
+    output = [LDL_output, HDL_output]
+    return output 
+chl_result = Cholesterol(120, 70)
 
 
 
@@ -24,4 +38,5 @@ print(num2)
 print(str)
 print(my_list)
 print(dict)
+print("Cholesterol Analysis:", chl_result)
 
